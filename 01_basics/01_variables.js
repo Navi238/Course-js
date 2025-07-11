eebc2a39afb20bff2a1353542d012aa786484593
Navneet Kumar <nkumarroy238@gmail.com>
2025-07-11 19:21:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553 // we can't change const keyword
+let accountEmail = "nkumar@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+perfer not to use var bcz of issue in block scope
+and function scope
+*/
