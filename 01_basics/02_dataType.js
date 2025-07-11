e88617cf83239507cab609c6918d63c0ab8b055d
Navneet Kumar <nkumarroy238@gmail.com>
2025-07-11 19:22:34 +0000

    Data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS codr as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Navneet")
+
+let name = "Navneet"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bollean => ture/false
+// null => standalone value
+// undefined => 
+// symbol => finding uniqueness
+
+
+// object
+
+// how to know Data typeof 
+
+console.log(typeof "Navneet");
+console.log(typeof 18 );
+console.log(typeof null); // interview question which datatype of null = object
+console.log(typeof undefined); // undefined
+
+
+
