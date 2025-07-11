238ec8c4de676ebd7abd0d8eb2845966f7ae55aa
Navneet Kumar <nkumarroy238@gmail.com>
2025-07-11 19:48:17 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,11 +15,54 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let bolleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(bolleanIsLoggedIn);
+// console.log(bolleanIsLoggedIn);
 
 // conversion of number to string
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//*********************** operations *************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // ** => power 
+// console.log(2/2);
+// console.log(2%2); // Reminder
+
+let str1 = "Navneet"
+let str2 = "hello"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ 2 + "2"); // if string in first than all command treat as a string
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// prifix and postfix => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+
+
