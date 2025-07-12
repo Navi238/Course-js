30d03fee2fbfceb8bd7602268f1ba1f756e09396
Navneet Kumar <nkumarroy238@gmail.com>
2025-07-12 08:19:45 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,32 @@
+// comparision operator
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1); //not equal
+
+// console.log("2" > 1);  
+// console.log("02" > 1);
+
+// compare always same data type this is not allow's different datatype to compare
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined > 0);
+
+// === [this sign check datatype also]
+
+console.log("2" === 1);
+
+
+
+
+
+
+
+
